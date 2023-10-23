1c8a28ae6523192f139d86181cea42c4a0bbf630
PlayerCoder1 <arlind.muslija@proton.me>
2023-10-23 14:44:03 +0200

    Finished work of loosecannon00 Set Idle Pose to 808 for localplayer Set Idle Pose for other players around you

---
+++ src/main/java/com/playercoder1/loosecannon00.java
@@ -0,0 +1,39 @@
+package com.playercoder1;
+
+
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.api.Client;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.Player;
+
+import javax.inject.Inject;
+
+@PluginDescriptor(
+		name = "loosecannon00",
+		description = "Does what loosecannon00 said",
+		tags = {"pose", "animation", "idle"}
+)
+public class loosecannon00 extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Subscribe
+	public void onGameTick(GameTick event)
+	{
+		Player[] players = client.getCachedPlayers();
+		if (players != null)
+		{
+			int animationId = 808;
+			for (Player player : players)
+			{
+				if (player != null)
+				{
+					player.setIdlePoseAnimation(animationId);
+				}
+			}
+		}
+	}
+}
